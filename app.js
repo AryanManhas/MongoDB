144434cd1137c4c5edd5b08990307d23a8925dcd
Aryan Manhas <2022A1R033@mietjammu.in>
2025-09-06 01:26:51 +0530

    app making using CRUD

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
 const express = require("express")
-
 const app = express();
+const path = require("path")
+const userModel = require("./models/user");
 
-const userModel = require("./usermodel");
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use(express.static(path.join(__dirname , "public")))
+app.set("view engine" , "ejs")
 
-app.get("/" , (req , res)=> {
-    res.send("Hey!")
+app.get("/" , (req , res)=>{
+    res.render("index");
+    
 })
 
-app.get("/create" , async (req , res)=> { // async to be put to nearest parent function
-    let createduser = await userModel.create( //as it is async js so we'll use await
-        {
-            name : "aryan",
-            username : "aryan",
-            email : "aryanmanhas767@gmail.com"
-        }
-    )
 
-    res.send(createduser)
+app.get("/read" , async (req , res)=>{
+    let users = await userModel.find();
+    res.render("read" , {users});
+    
 })
 
-app.get("/update" , async (req , res)=> { // async to be put to nearest parent function
-    // userModel.findondUpdate( findone , update , {new : true}) the ways of writing
-    let updateduser = await userModel.findOneAndUpdate( {username: 'aryan' }, {username: 'aryan manhas'} , {new : true})
-    res.send(updateduser);
+
+app.get("/delete/:id" , async (req , res)=>{
+    let users = await userModel.findOneAndDelete({_id : req.params.id});
+    res.redirect("/read")
+    
 })
 
-app.get("/read" , async (req , res)=> { // async to be put to nearest parent function
-    // let users = await userModel.find({username:"aryan"}) for individual read always returns an array and gives empty if one user is not present and it we use .findOne the it will give null findOne gives us object findOne always find the first user
-    let users = await userModel.find()
-    res.send(users);
-})
+app.post("/create" , async (req , res)=>{
+let {name , email , image} = req.body;
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image
+    });
 
-app.get("/delete" , async (req , res)=> { // async to be put to nearest parent function
-    // let users = await userModel.find({username:"aryan"}) for individual read always returns an array and gives empty if one user is not present and it we use .findOne the it will give null findOne gives us object findOne always find the first user
-    let deleteduser = await userModel.findOneAndDelete()
-    res.send(deleteduser);
+    res.redirect("/read")
 })
-
 
 app.listen(3000);
